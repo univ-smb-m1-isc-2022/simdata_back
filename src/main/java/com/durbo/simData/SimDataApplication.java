f1f300e9c7d4805eb26e74db3c292c76b223f757
Durb0 <58364246+Durb0@users.noreply.github.com>
2023-02-11 10:46:05 +0100

    Initial commit

---
+++ src/main/java/com/durbo/simData/SimDataApplication.java
@@ -0,0 +1,13 @@
+package com.durbo.simData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durbo/simData/SimDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durbo.simData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
