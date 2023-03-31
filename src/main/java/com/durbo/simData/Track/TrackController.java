762f22c0e4f9322394f12692f9b76948481d097d
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2023-03-31 17:38:57 +0200

    in progress

--- src/main/java/com/durbo/simData/Track/TrackController.java
+++ src/main/java/com/durbo/simData/Track/TrackController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 @Slf4j
@@ -25,6 +27,23 @@ public class TrackController {
     public Track getTrack(@PathVariable String name) {
         log.info("Getting track");
         return (Track) trackService.getBy("Track","name", name).get(0);
+    }
+
+    @GetMapping("/tracks/region/{value}")
+    public ArrayList<Object> getTracksByRegion(@PathVariable String value) throws MalformedURLException {
+        log.info("Getting tracks by region");
+        //first get all countries in region
+        //then get all tracks in those countries
+        //api : https://restcountries.com/v3.1/
+        try {
+            URL url = new URL("https://restcountries.com/v3.1/region/" + value);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        return trackService.getBy("Track", "region", value);
     }
 
     @GetMapping("/tracks/{attribute}/{value}")
